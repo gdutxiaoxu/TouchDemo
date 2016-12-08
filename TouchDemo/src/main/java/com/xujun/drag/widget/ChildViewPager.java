ada7dd569b0a12ce2684aea839c742c908c881ba
xujun <973243764@qq.com>
2016-12-08 22:10:47 +0800

    答复一些疑问

--- TouchDemo/src/main/java/com/xujun/drag/widget/ChildViewPager.java
+++ TouchDemo/src/main/java/com/xujun/drag/widget/ChildViewPager.java
@@ -34,8 +34,10 @@ public class ChildViewPager extends ViewPager {
                 curPosition = this.getCurrentItem();
                 int count = this.getAdapter().getCount();
                 Log.i(TAG, "curPosition:=" +curPosition);
+//                全部由孩子拦截触摸事件
+/*                getParent().requestDisallowInterceptTouchEvent(true);*/
                 // 当当前页面在最后一页和第0页的时候，由父亲拦截触摸事件
-                if (curPosition == count - 1|| curPosition==0) {
+                if (curPosition == count - 1 || curPosition == 0) {
                     getParent().requestDisallowInterceptTouchEvent(false);
                 } else {//其他情况，由孩子拦截触摸事件
                     getParent().requestDisallowInterceptTouchEvent(true);
